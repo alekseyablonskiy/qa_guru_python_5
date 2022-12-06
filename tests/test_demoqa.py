662bf68a975888f91282f5b5f384a460d909b39a
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2022-12-06 12:25:35 +0300

    added picture

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,33 +1,39 @@
+import os
+
 from selene.support.shared import browser
 from selene import have
 
 
 def test_filled_form():
     browser.open('https://demoqa.com/automation-practice-form')
-    #user name
+    # username
     browser.element('#firstName').type('Aleksey')
     browser.element('#lastName').type('Yablonskiy')
-    #personal data
+    # personal data
     browser.element('#userEmail').type('alekseyablonskiy@gmail.com')
     browser.element('[for=gender-radio-1]').click()
     browser.element('#userNumber').type('123456789012345')
-    #user birthday
+    # user birthday
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('[value="10"]').click()
     browser.element('.react-datepicker__year-select').click()
     browser.element('[value="1996"]').click()
     browser.element('.react-datepicker__day--027').click()
-    #subject and hobbies
+    # subject and hobbies
     browser.element('#subjectsInput').type('English').press_enter()
-    # User address
+    browser.element('[for=hobbies-checkbox-3').click()
+    # upload picture
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'files/picture.jpg')))
+    # user address
     browser.element('#currentAddress').type("Minsk")
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#react-select-4-input').type('Noida').press_enter()
 
-    #submitting form
+    # submitting form
     browser.element('#submit').click()
-    #result
+    # result
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.element('.table').should(have.text(
         'Aleksey Yablonskiy' and
@@ -36,6 +42,8 @@ def test_filled_form():
         '123456789012345' and
         '27 October, 1996' and
         'English' and
+        'Music' and
+        'picture.jpg' and
         'Minsk' and
         'NCR Noida'
     ))
